216dadff4d6bcc4907303cdf128a6c08a9a345ae
Mateusz Magda <realgs@gmail.com>
2018-06-15 19:58:08 +0200

    Standard tests passed.

--- stats.js
+++ stats.js
@@ -1,10 +1,3 @@
-const getAverage = (series) => {
-	var sum = 0;
-	for ( var i = 0; i < series.length; i++ ){
-		sum += series[i].visits;
-	}
-	return sum / series.length;
-};
 
 let oneWeek = [
     { date: new Date('2018-01-01'), visits: 32 },
@@ -24,10 +17,58 @@ let secondWeek = [
     { date: new Date('2018-01-06'), visits: 64 },
     { date: new Date('2018-01-07'), visits: 44 },
 ];
+let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 let twoWeeks = oneWeek.concat(secondWeek);
-console.log(oneWeek);
-console.log(twoWeeks);
-var avg = getAverage(oneWeek);
+
+const getAverage = (series, weekdays) => {
+	if ( weekdays ){
+		var sum = new Array(7).fill(0);
+		for ( var i = 0; i < series.length; i++ ){
+			sum[series[i].date.getDay()] += series[i].visits;
+		}
+		return {
+	      Monday: {
+	        averageVisits: sum[1],
+	      },
+	      Tuesday: {
+	        averageVisits: sum[2],
+	      },
+	      Wednesday: {
+	        averageVisits: sum[3],
+	      },
+	      Thursday: {
+	        averageVisits: sum[4],
+	      },
+	      Friday: {
+	        averageVisits: sum[5],
+	      },
+	      Saturday: {
+	        averageVisits: sum[6],
+	      },
+	      Sunday: {
+	        averageVisits: sum[0],
+	      },
+	    };
+		// return { "Monday": { "averagevisits": sum[1], }, 
+		// 		"Tuesday": { "averagevisits": sum[2], },
+		// 		"Wednesday": { "averagevisits": sum[3], },
+		// 		"Thursday": { "averagevisits": sum[4], },
+		// 		"Friday": { "averagevisits": sum[5], },
+		// 		"Saturday": { "averagevisits": sum[6], },
+		// 		"Sunday": { "averagevisits": sum[0], }
+		// 		 };
+
+	} else {
+		var sum = 0;
+		for ( var i = 0; i < series.length; i++ ){
+			sum += series[i].visits;
+		}
+		return { "averageVisits": (sum / series.length) };
+	}
+};
+
+var avg = getAverage(oneWeek, true);
 console.log(avg);
 
 
