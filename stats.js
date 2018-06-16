a1b48ae668dbf5671d24e189bf00f0c82e771db3
Mateusz Magda <realgs@gmail.com>
2018-06-16 12:42:53 +0200

    Code cleared up. Tests moved to stats.spec.js. Tests added: checks empty values, empty days between records, series concatenation.

--- stats.js
+++ stats.js
@@ -1,88 +1,57 @@
-let mSecInDay = 3600 * 24 * 1000;
-let oneWeek = [
-    { date: new Date('2018-01-05'), visits: 54 },
-    { date: new Date('2018-01-01'), visits: 32 },
-    { date: new Date('2018-01-02'), visits: 82 },
-    { date: new Date('2018-01-03'), visits: 74 },
-    { date: new Date('2018-01-04'), visits: 35 },
-    { date: new Date('2018-01-06'), visits: 64 },
-    { date: new Date('2018-01-07'), visits: 44 },
-];
-let secondWeek = [
-    { date: new Date('2018-01-01'), visits: 32 },
-    { date: new Date('2018-01-02'), visits: 82 },
-    { date: new Date('2018-01-03'), visits: 74 },
-    { date: new Date('2018-01-04'), visits: 35 },
-    { date: new Date('2018-01-05'), visits: 54 },
-    { date: new Date('2018-01-06'), visits: 64 },
-    { date: new Date('2018-01-07'), visits: 44 },
-];
-let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let milliSecInDay = 3600 * 24 * 1000;
 
-let twoWeeks = oneWeek.concat(secondWeek);
-
-function countCertainDays( days, d0, d1 ) {
-  var nDays = 1 + Math.round((d1-d0) / mSecInDay);
-  var sum = function(a,b) {
-    return a + Math.floor( (nDays+(d0.getDay()+6-b) % 7 ) / 7 ); };
-  return days.reduce(sum,0);
+function countCertainDay( day, d0, nDays ) {
+	return Math.floor( ( nDays + ( d0.getDay() + 6 - day ) % 7 ) / 7 );
 }
-const getAverage = (series, weekdays) => {
-	if ( weekdays ){
-		var sum = new Array(7).fill(0);
-		var oldestDate = Infinity;
-		var newestDate = -Infinity;
-		for ( var i = 0; i < series.length; i++ ){
-			let whichDay = series[i].date.getDay();
-			sum[whichDay] += series[i].visits;
-			oldestDate = series[i].date < oldestDate ? series[i].date : oldestDate;
-			newestDate = series[i].date > newestDate ? series[i].date : newestDate;
-		}
-		let nDays = countCertainDays([0, 1, 2, 3, 4, 5, 6], oldestDate, newestDate);
-		console.log(countCertainDays([0, 1, 2, 3, 4, 5, 6], oldestDate, newestDate));
-		return {
-	      Monday: {
-	        averageVisits: sum[1] / countCertainDays([1], oldestDate, newestDate),
-	      },
-	      Tuesday: {
-	        averageVisits: sum[2] / countCertainDays([2], oldestDate, newestDate),
-	      },
-	      Wednesday: {
-	        averageVisits: sum[3] / countCertainDays([3], oldestDate, newestDate),
-	      },
-	      Thursday: {
-	        averageVisits: sum[4] / countCertainDays([4], oldestDate, newestDate),
-	      },
-	      Friday: {
-	        averageVisits: sum[5] / countCertainDays([5], oldestDate, newestDate),
-	      },
-	      Saturday: {
-	        averageVisits: sum[6] / countCertainDays([6], oldestDate, newestDate),
-	      },
-	      Sunday: {
-	        averageVisits: sum[0] / countCertainDays([0], oldestDate, newestDate),
-	      },
-	    };
+const getAverage = (series, weekdayMode) => {
+	if ( !series ){
+		return { "averageVisits": 0 };;
 	} else {
-		var sum = 0;
-		var oldestDate = series[0].date;
-		var newestDate = series[series.length - 1].date;
+		var startDate = Infinity;
+		var endDate = -Infinity;
 		for ( var i = 0; i < series.length; i++ ){
-			sum += series[i].visits;
-			oldestDate = series[i].date < oldestDate ? series[i].date : oldestDate;
-			newestDate = series[i].date > newestDate ? series[i].date : newestDate;		
+			startDate = series[i].date < startDate ? series[i].date : startDate;
+			endDate = series[i].date > endDate ? series[i].date : endDate;
 		}
-		let nDays = ( newestDate - oldestDate ) / mSecInDay + 1;
-		return { "averageVisits": (sum / nDays) };
+		let nDays = ( endDate - startDate ) / milliSecInDay + 1;
+
+		if ( weekdayMode ){
+			var sum = new Array(7).fill(0);
+			for ( var i = 0; i < series.length; i++ ){
+				let currentDay = series[i].date.getDay();
+				sum[currentDay] += series[i].visits;
+			}
+			return {
+		      Monday: {
+		        averageVisits: sum[1] / countCertainDay([1], startDate, nDays),
+		      },
+		      Tuesday: {
+		        averageVisits: sum[2] / countCertainDay([2], startDate, nDays),
+		      },
+		      Wednesday: {
+		        averageVisits: sum[3] / countCertainDay([3], startDate, nDays),
+		      },
+		      Thursday: {
+		        averageVisits: sum[4] / countCertainDay([4], startDate, nDays),
+		      },
+		      Friday: {
+		        averageVisits: sum[5] / countCertainDay([5], startDate, nDays),
+		      },
+		      Saturday: {
+		        averageVisits: sum[6] / countCertainDay([6], startDate, nDays),
+		      },
+		      Sunday: {
+		        averageVisits: sum[0] / countCertainDay([0], startDate, nDays),
+		      },
+		    };
+		} else {
+			var sum = 0;
+			for ( var i = 0; i < series.length; i++ ){
+				sum += series[i].visits;	
+			}
+			return { "averageVisits": (sum / nDays) };
+		}
 	}
 };
-
-var avg = getAverage(oneWeek, true);
-console.log(avg);
-var avg = getAverage(secondWeek, true);
-console.log(avg);
-var avg = getAverage(twoWeeks, true);
-console.log(avg);
-
 
 module.exports = getAverage;
--- stats.spec.js
+++ stats.spec.js
@@ -10,13 +10,57 @@ describe('getAverage', () => {
     { date: new Date('2018-01-06'), visits: 64 },
     { date: new Date('2018-01-07'), visits: 44 },
   ];
+  let oneWeekSwitched = [
+    { date: new Date('2018-01-05'), visits: 54 },
+    { date: new Date('2018-01-01'), visits: 32 },
+    { date: new Date('2018-01-02'), visits: 82 },
+    { date: new Date('2018-01-03'), visits: 74 },
+    { date: new Date('2018-01-04'), visits: 35 },
+    { date: new Date('2018-01-06'), visits: 64 },
+    { date: new Date('2018-01-07'), visits: 44 },
+  ];
+  let oneMonthAlmostEmpty = [
+    { date: new Date('2018-06-01'), visits: 6 },
+    { date: new Date('2018-06-02'), visits: 1 },
+    { date: new Date('2018-06-06'), visits: 67 },
+    { date: new Date('2018-06-14'), visits: 17 },
+    { date: new Date('2018-06-23'), visits: 18 },
+    { date: new Date('2018-06-27'), visits: 21 },
+    { date: new Date('2018-06-29'), visits: 19 },
+  ];
+  let twoWeeks = oneWeek.concat(oneWeekSwitched);
 
+  test('average of null dataset', () => {
+    expect(getAverage(null)).toEqual({
+      averageVisits: 0,
+    });
+  });
+
+    test('average of null dataset', () => {
+    expect(getAverage(undefined)).toEqual({
+      averageVisits: 0,
+    });
+  });
+  test('average of empty string dataset', () => {
+    expect(getAverage("")).toEqual({
+      averageVisits: 0,
+    });
+  });
   test('average of one week', () => {
     expect(getAverage(oneWeek)).toEqual({
       averageVisits: 55,
     });
   });
-
+  test('average of one week with Monday and Friday switched.', () => {
+    expect(getAverage(oneWeekSwitched)).toEqual({
+      averageVisits: 55,
+    });
+  });
+  test('average of two week concatenated', () => {
+    expect(getAverage(twoWeeks)).toEqual({
+      averageVisits: 110,
+    });
+  });
   test('week days average of one week', () => {
     expect(getAverage(oneWeek, true)).toEqual({
       Monday: {
@@ -39,6 +83,31 @@ describe('getAverage', () => {
       },
       Sunday: {
         averageVisits: 44,
+      },
+    });
+  });
+    test('week days average of one, almost empty month', () => {
+    expect(getAverage(oneMonthAlmostEmpty, true)).toEqual({
+      Monday: {
+        averageVisits: 0,
+      },
+      Tuesday: {
+        averageVisits: 0,
+      },
+      Wednesday: {
+        averageVisits: 22,
+      },
+      Thursday: {
+        averageVisits: 4.25,
+      },
+      Friday: {
+        averageVisits: 5,
+      },
+      Saturday: {
+        averageVisits: 4.75,
+      },
+      Sunday: {
+        averageVisits: 0,
       },
     });
   });
